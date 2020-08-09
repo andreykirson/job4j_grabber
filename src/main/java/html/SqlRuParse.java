7092841ab78f241cc0ef63b68303c8487ceaeec3
andreykirson <a.s.kirson@gmail.com>
2020-08-09 22:19:13 +1000

    2.1. Преобразование даты [#302198]

--- src/main/java/html/SqlRuParse.java
+++ src/main/java/html/SqlRuParse.java
@@ -4,14 +4,25 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Locale;
+
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
         Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
         Elements table = doc.getElementsByClass("forumTable").get(0).getElementsByTag("tr");
-        for (int i = 4; i < table.size(); i++) {
+        for (int i = 5; i < table.size(); i++) {
             String vacancy = table.get(i).getElementsByClass("postslisttopic").text();;
             String date = table.get(i).getElementsByTag("td").get(5).text();
-            System.out.println(vacancy + " " + date);
+
+            SimpleDateFormat formatter = new SimpleDateFormat("d MMM yy", new Locale("ru"));
+            String[] monthAndYear = date.split(",");
+            Date dateMontYear = formatter.parse(monthAndYear[0]);
+            System.out.println(dateMontYear);
         }
     }
 }
