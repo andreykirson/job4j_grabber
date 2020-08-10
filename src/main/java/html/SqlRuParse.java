c3eaa29e695e52c78388c49e7db18c399bde3bdc
andreykirson <a.s.kirson@gmail.com>
2020-08-10 23:01:07 +1000

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#302194]

--- src/main/java/html/SqlRuParse.java
+++ src/main/java/html/SqlRuParse.java
@@ -3,26 +3,22 @@ package html;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
-
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.Locale;
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements table = doc.getElementsByClass("forumTable").get(0).getElementsByTag("tr");
-        for (int i = 5; i < table.size(); i++) {
-            String vacancy = table.get(i).getElementsByClass("postslisttopic").text();;
-            String date = table.get(i).getElementsByTag("td").get(5).text();
-
-            SimpleDateFormat formatter = new SimpleDateFormat("d MMM yy", new Locale("ru"));
-            String[] monthAndYear = date.split(",");
-            Date dateMontYear = formatter.parse(monthAndYear[0]);
-            System.out.println(dateMontYear);
+        int page;
+        for (page = 1; page <= 3; page++) {
+            Document doc = Jsoup.connect(String.format("https://www.sql.ru/forum/job-offers/%s", page)).get();
+                Elements table = doc.getElementsByClass("forumTable").get(0).getElementsByTag("tr");
+                for (int i = 4; i < table.size(); i++) {
+                    String vacancy = table.get(i).getElementsByClass("postslisttopic").text();
+                    String date = table.get(i).getElementsByTag("td").get(5).text();
+                    System.out.println(vacancy + " " + date);
+                }
         }
     }
 }
+
