559067d22c87b364a5f8c9a3748ce60c9a38d1b7
andreykirson <a.s.kirson@gmail.com>
2020-08-19 21:31:53 +1000

    5. PsqlStore [#302193]

--- src/main/java/html/Post.java
+++ src/main/java/html/Post.java
@@ -63,7 +63,7 @@ public class Post {
     public boolean equals(Object o) {
         if (this == o) {
             return true;
-        };
+        }
         if (o == null || getClass() != o.getClass()) {
             return false;
         }
